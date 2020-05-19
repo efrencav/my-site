9840529368256d2ad2f490c723df0af6e35d11b8
efrencav <efren@abstract-creative.com>
2020-05-19 10:44:06 -0500

    revised footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -21,7 +21,7 @@ class Footer extends Component {
                             <div className="col-lg-6">
                                 <div className="footer-left">
                                     <div className="inner">
-                                        <span>Ready To Do This</span>
+                                        <span>I'm Ready To Do This</span>
                                         <h2>Let's get <br /> to work</h2>
                                         <a className="rn-button-style--2" href="/#contact">
                                             <span>Contact Me</span>
